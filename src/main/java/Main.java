075766252293cc7bff66e1a61693a8f8f4ff29d8
imlishant <lishantsahu@gmail.com>
2025-11-26 13:43:44 +0530

    Fix invalid command test failure.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
-        System.out.println(command + "command not found");
+        System.out.println(command + ": command not found");
         scanner.close();
     }
 }
