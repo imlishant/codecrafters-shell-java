eeb667c3d194f6d1555dd655f88180d8bbfaf75c
imlishant <lishantsahu@gmail.com>
2025-11-26 18:14:16 +0530

    Add type command to print builtin commands in REPL.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,14 @@ public class Main {
                     System.exit(0);
                 } else if (command.startsWith("echo ")) {
                     System.out.println(command.substring(5));
+                } else if (command.startsWith("type ")) {
+                   String cmd = command.substring(5);
+                   if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")) {
+                          System.out.println(cmd + " is a shell builtin");
+                     } else {
+                          System.out.println(cmd + ": not found");
+                        //   System.exit(0);
+                   } 
                 } else {
                     System.out.println(command + ": command not found");
                 }
